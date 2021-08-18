2cb4f8f5f51757fe365e860a14ede4048daff16f
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-18 14:00:16 -0500

    Initial Commit

---
+++ DiceSim/DiceSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceSimApp.swift
+//  DiceSim
+//
+//  Created by Antonio Vega on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
